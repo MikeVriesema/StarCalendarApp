3f5ccdbf09130f2aaf675fe707132693bc3d727c
Mike Vriesema <17212359@studentmail.ul.ie>
2019-03-08 02:13:28 +0000

    -Added app icon -Added app background -Added several drawables to smooth out button corners and for various shape elements -Added popup window upon click of date in calendar that will display a particular celestial event if available -Tidied up

--- app/src/main/java/mikevriesema/ul/starcalendar/Calendar.java
+++ app/src/main/java/mikevriesema/ul/starcalendar/Calendar.java
@@ -1,6 +1,9 @@
 package mikevriesema.ul.starcalendar;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.annotation.NonNull;
@@ -19,13 +22,18 @@ public class Calendar extends Activity {
 
     CalendarView calendar;
     TextView dateplan;
+    TextView close;
+    Dialog popDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        popDialog = new Dialog(this);
         calendar = (CalendarView) findViewById(R.id.calendar);
         dateplan = (TextView) findViewById(R.id.date);
+
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -34,9 +42,36 @@ public class Calendar extends Activity {
                 String events = "";
                 String newDate = dayOfMonth + " of " + months[month] + " " + year + "\n" + events;
                 dateplan.setText(newDate);
+
+                popUpEvent();
             }
         });
+        /*
+        * Calendar cal = Calendar.getInstance();
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setType("vnd.android.cursor.item/event");
+            intent.putExtra("beginTime", cal.getTimeInMillis());
+            intent.putExtra("allDay", true);
+            intent.putExtra("rrule", "FREQ=YEARLY");
+            intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+            intent.putExtra("title", "A Test Event from android app");
+            startActivity(intent);
+         */
+    }
 
+
+    public void popUpEvent(){
+        popDialog.setContentView(R.layout.activity_event_info);
+        close = (TextView) popDialog.findViewById(R.id.close);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popDialog.dismiss();
+            }
+        });
+        popDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        popDialog.show();
     }
 
 }
--- app/src/main/java/mikevriesema/ul/starcalendar/Information.java
+++ app/src/main/java/mikevriesema/ul/starcalendar/Information.java
@@ -1,12 +1,17 @@
 package mikevriesema.ul.starcalendar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 
 public class Information extends Activity {
@@ -15,7 +20,7 @@ public class Information extends Activity {
     String[] starNames;
     TextView databaseView;
     TextView stardescriptions;
-
+    ImageView starImage;
     StarsDB starDB;
 
     /*/////////////////////////////////
@@ -30,9 +35,9 @@ public class Information extends Activity {
         starSpinner = (Spinner) findViewById(R.id.starspinner);
         databaseView = (TextView) findViewById(R.id.databasevalues);
         stardescriptions = (TextView) findViewById(R.id.stardescriptions);
+        starImage = (ImageView) findViewById(R.id.starimage);
 
         starDB = new StarsDB(getApplicationContext());
-
 
         starNames = starDB.getStarNames();
         ArrayAdapter<String> starAdapter = new ArrayAdapter<String>(getBaseContext(), R.layout.list_item, starNames);
@@ -57,6 +62,16 @@ public class Information extends Activity {
 
     }
 
+    private void loadImageFromStorage(String path)
+    {
+        try {
+            Bitmap b = BitmapFactory.decodeFile(path);
+            ImageView img=(ImageView)findViewById(R.id.starimage);
+            img.setImageBitmap(b);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
 
